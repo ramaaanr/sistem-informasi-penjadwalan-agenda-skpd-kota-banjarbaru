f87e98d98e5842b7d1c55e28619d8a130e991507
Rama Noor Rizki <ramanoorrizki@gmail.com>
2023-12-23 21:25:22 +0800

    chore: add migration for table events and users

---
+++ database/migrations/2023_12_23_132201_create_table_events.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100)->nullable(false);
+            $table->string('tempat', 100)->nullable(false);
+            $table->string('dihadiri', 100)->nullable(false);
+            $table->string('pakaian', 100)->nullable(false);
+            $table->dateTime('start_event')->nullable(false);;
+            $table->dateTime('end_event')->nullable(true);;
+            $table->string('keterangan')->nullable(false);;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
